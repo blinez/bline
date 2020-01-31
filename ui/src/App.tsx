db611451db4e2d52273e31514d55aa54c5641bf0
bs <stone.bastian@gmail.com>
2020-01-31 17:32:27 +0100

    introduce eslint and prettier

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,24 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to bline (G and A were here)!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Welcome to bline (G and A were here)!!</p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
