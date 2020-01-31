6b358c1711f7e5030470b057abc953767e66af94
bs <stone.bastian@gmail.com>
2020-01-31 17:42:56 +0100

    trigger workflow

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Welcome to bline (G and A were here)!!</p>
+                <p>Welcome to bline (G and A and B were here)!!</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
